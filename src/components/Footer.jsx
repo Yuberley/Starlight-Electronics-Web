c9bd94433b6c49e65401fb0069e729f272c8732d
ControlAV <122927481+ControlAV@users.noreply.github.com>
2023-10-06 15:45:44 -0500

    Update Footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -54,7 +54,7 @@ export const Footer = () => {
                             <img className='ml-3' src={ avixamember } alt="Avixamember con starlightelectronics" title='Avixamember con starlight electronics' />
                         <figure className='w-36 border-l ml-6'>
                             <img className='ml-3' src={ incontec } alt="incontec con starlightelectronics" title='incontec con starlight electronics' />
-                        
+                        </figure>
 
                     </span>
                 </div>
