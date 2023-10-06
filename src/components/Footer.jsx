b4dcfaac25acaf318274edd219a2ab66e09fac5d
ControlAV <122927481+ControlAV@users.noreply.github.com>
2023-10-06 15:48:23 -0500

    Update Footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -8,7 +8,7 @@ import {
   } from 'react-icons/fa';
 
 import avixamember from '../assets/img/content/avixamember.png';
-import incontec from '../assets/img/content/2avixamember.png';
+
 
 export const Footer = () => {
     return (
@@ -52,8 +52,6 @@ export const Footer = () => {
 
                         <figure className='w-36 border-l ml-6'>
                             <img className='ml-3' src={ avixamember } alt="Avixamember con starlightelectronics" title='Avixamember con starlight electronics' />
-                        <figure className='w-36 border-l ml-6'>
-                            <img className='ml-3' src={ incontec } alt="incontec con starlightelectronics" title='incontec con starlight electronics' />
                         </figure>
 
                     </span>
