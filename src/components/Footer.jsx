0a7ed9855889155dce526d78d4ba2bc96459bd85
ControlAV <122927481+ControlAV@users.noreply.github.com>
2023-10-06 17:41:38 -0500

    Update Footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import React from 'react';
 import {
   FaWhatsapp,
   FaInstagram,
@@ -12,12 +11,12 @@ import incontec from '../assets/img/content/incontec.png';
 
 export const Footer = () => {
   return (
-    <footer className="text-gray-700 bg-white mt-16">
-      <div className="container mx-auto py-8 px-4 sm:px-6 lg:px-8">
-        <div className="flex flex-col items-center justify-center sm:flex-row">
-          <a href="#" className="text-xl font-black leading-none text-gray-900 select-none logo uppercase"> Starlight Electronics </a>
-          <p className="mt-4 text-sm text-gray-500 sm:ml-4 sm:pl-4 sm:border-l sm:border-gray-200 sm:mt-0">© 2022 - Todos los derechos reservados</p>
-          <div className="flex items-center mt-4 space-x-5 sm:ml-auto sm:mt-0">
+    <footer className="inset-x-0 bottom-0 text-gray-700 bg-white body-font mt-16">
+      <div className="flex flex-col items-center justify-center px-8 py-8 mx-auto max-w-7xl sm:flex-row">
+        <a href="#" className="text-xl font-black leading-none text-gray-900 select-none logo uppercase"> Starlight Electronics </a>
+        <p className="mt-4 text-sm text-gray-500 sm:mt-0 sm:ml-4 sm:pl-4 sm:border-l sm:border-gray-200">© 2022 - Todos los derechos reservados</p>
+        <span className="inline-flex justify-center mt-4 space-x-5 sm:mt-0 sm:justify-start">
+          <div className="flex flex-col sm:flex-row mt-4 space-x-5">
             <a
               href="https://api.whatsapp.com/message/S5CMKVFJ7PDSM1?autoload=1&app_absent=0"
               className="text-gray-400 hover:text-gray-500"
@@ -51,15 +50,16 @@ export const Footer = () => {
               <FaLinkedin size={30} />
             </a>
           </div>
-          <div className="flex items-center mt-4 space-x-5 sm:ml-6">
-            <figure className='w-36'>
-              <img className='ml-3' src={avixamember} alt="Avixamember con starlightelectronics" title='Avixamember con Starlight Electronics' style={{ maxWidth: '100%' }} />
+
+          <div className="flex flex-col sm:flex-row mt-4 space-x-5">
+            <figure className='w-36 border-l ml-6'>
+              <img className='ml-3' src={avixamember} alt="Avixamember con starlightelectronics" title='Avixamember con starlight electronics' style={{ maxWidth: '100%' }} />
             </figure>
-            <figure className='w-36'>
-              <img className='ml-3' src={incontec} alt="Incontec con starlightelectronics" title='Incontec con Starlight Electronics' style={{ maxWidth: '100%' }} />
+            <figure className='w-36 border-l ml-6'>
+              <img className='ml-3' src={incontec} alt="Incontec con starlightelectronics" title='Incontec con starlight electronics' style={{ maxWidth: '100%' }} />
             </figure>
           </div>
-        </div>
+        </span>
       </div>
     </footer>
   );
