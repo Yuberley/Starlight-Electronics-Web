8eac03e71dd6a9f9b8556b44a253b66bbbce39a7
ControlAV <122927481+ControlAV@users.noreply.github.com>
2023-10-06 16:30:22 -0500

    Update Footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -12,7 +12,7 @@ import incontec from '../assets/img/content/incontec.png';
 
 export const Footer = () => {
     return (
-            <footer className="inset-x-0 bottom-0 text-gray-700 bg-white body-font mt-18">
+            <footer className="inset-x-0 bottom-0 text-gray-900 bg-white body-font mt-16">
                 <div className="flex flex-col items-center px-8 py-8 mx-auto max-w-7xl sm:flex-row">
                     <a href="#" className="text-xl font-black leading-none text-gray-900 select-none logo uppercase"> Starlight Electronics </a>
                     <p className="mt-4 text-sm text-gray-500 sm:ml-4 sm:pl-4 sm:border-l sm:border-gray-200 sm:mt-0">© 2022 - Todos los derechos reservados</p>
